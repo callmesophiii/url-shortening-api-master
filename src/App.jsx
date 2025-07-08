7f10e7d6a48f9b4423249b7285558ebda3af0e0c
Sophia <callmesophiii@gmail.com>
2025-07-08 15:19:27 -0400

    updated paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Header from '../../url-shortening-api-master/src/components/Header';
-import Hero from '../../url-shortening-api-master/src/components/Hero';
-import ShortenForm from '../../url-shortening-api-master/src/components/ShortenForm';
-import LinkList from '../../url-shortening-api-master/src/components/LinkList';
-import Statistics from '../../url-shortening-api-master/src/components/Statistics';
-import CTA from '../../url-shortening-api-master/src/components/CTA';
-import Footer from '../../url-shortening-api-master/src/components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import ShortenForm from './components/ShortenForm';
+import LinkList from './components/LinkList';
+import Statistics from './components/Statistics';
+import CTA from './components/CTA';
+import Footer from './components/Footer';
 import './index.css';
 
 const App = () => {
